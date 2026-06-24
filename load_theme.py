b446b61b3d023bcba0d6bcbffaf812a6ea4654bf
nihiL7331 <pujanekp6@gmail.com>
2026-06-24 12:48:02 +0200

    feat(core): load theme from command-line argument

--- load_theme.py
+++ load_theme.py
@@ -1,11 +1,16 @@
 import json
+import sys
 import os
 import shutil
 import subprocess
 
+if len(sys.argv) != 2:
+    print("Usage: python3 load_theme.py <theme.json>")
+    exit(1)
+
 DOTFILES = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(DOTFILES, 'theme.json')) as f:
+with open(os.path.join(DOTFILES, sys.argv[1])) as f:
     theme = json.load(f)
 
 def rgb2argb_pfd(col_str):
